797e58c7fa18f4e3ca502ef7bf64e7098992589d
thereclaimer <sam.davenport@thereclaimergame.com>
2026-05-16 12:55:01 -0500

    heap operations

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -4,6 +4,10 @@
 
 namespace sld {
 
+    //--------------------------------------------------------------------
+    // API METHODS
+    //--------------------------------------------------------------------
+
     SLD_ALGORITHMS_API_FUNC u32
     huffman_context_memory_requirement(
         const u32 data_size) {
@@ -50,14 +54,16 @@ namespace sld {
             mem_ptr   != NULL
         );
 
-        memset(mem_ptr, 0, mem_size);
+        zero_memory(mem_ptr, mem_size);
+        
         auto hfmn = (huffman_context*)mem_ptr;
-        hfmn->tree       =            (huffman_tree*)(mem_ptr + HUFFMAN_STRUCT_CONTEXT);
-        hfmn->freq_table = (huffman_frequency_table*)(((void*)hfmn->tree)         + HUFFMAN_STRUCT_TREE);
-        hfmn->data_ptr   =                    (byte*)(((void*)hfmn->symbol_array) + HUFFMAN_STRUCT_SYMBOL_TABLE);
+        hfmn->tree       =            (huffman_tree*)(mem_ptr                     + HUFFMAN_STRUCT_SIZE_CONTEXT);
+        hfmn->heap       =            (huffman_heap*)(((void*)hfmn->heap)         + HUFFMAN_STRUCT_SIZE_TREE);
+        hfmn->freq_table = (huffman_frequency_table*)(((void*)hfmn->tree)         + HUFFMAN_STRUCT_SIZE_HEAP);
+        hfmn->data_ptr   =                    (byte*)(((void*)hfmn->symbol_array) + HUFFMAN_STRUCT_SIZE_FREQ_TABLE);
         hfmn->data_size  = data_size;
 
-        return(huffman_context);
+        return(hfmn);
     }
 
     huffman_result
@@ -72,42 +78,157 @@ namespace sld {
             data_ptr  != NULL
         );
 
-        // reset the symbol table and tree
-        memset((void*)hffmn->freq_table, 0,   HUFFMAN_STRUCT_FREQ_TABLE);
-        memset((void*)hffmn->tree,         0, HUFFMAN_STRUCT_TREE);
+        // build the frequency table
+        huffman_frequency_table_build(
+            hffmn->freq_table,
+            data_size,
+            data_ptr
+        );
+
+        // initialize the tree
+        
+    }
+
+
+
+    SLD_ALGORITHMS_API_FUNC u32          huffman_data_stream_memory_size    (const huffman_context* hffmn);
+    SLD_ALGORITHMS_API_FUNC data_stream* huffman_data_stream_create         (const huffman_context* hffmn);
+    SLD_ALGORITHMS_API_FUNC data_stream* huffman_data_stream_memory_init    (const u32 data_stream_size, const u32 mem_size, void* mem_ptr);
+    SLD_ALGORITHMS_API_FUNC void         huffman_copy_data_stream           (const huffman_context* hffmn, data_stream* ds);
+
+    //--------------------------------------------------------------------
+    // INTERNAL METHODS
+    //--------------------------------------------------------------------
+
+    SLD_ALGORITHMS_INTERNAL void
+    huffman_frequency_table_build(
+        huffman_frequency_table* table,
+        const byte*              data_ptr,
+        const u32                data_size) {
+
+        assert(
+            table     != NULL &&
+            data_ptr  != NULL &&
+            data_size != 0
+        );
+
+        memset((void*)table, 0, HUFFMAN_STRUCT_FREQ_TABLE);
 
         // count the symbols
-        huffman_frequency_table* freq_table = hfmn->freq_table;
         for (
             u32 index = 0;
                 index < data_size;
               ++index) {
 
             const byte symbol = data_ptr[index];
-            ++freq_table->sparse_array_frequencies[symbol];
+            ++table->sparse_array_frequencies[symbol];
         }
 
         // add the symbols to the dense array
         for (
-            byte symbol = 0;
-                 symbol < HUFFMAN_MAX_SYMBOLS;
-               ++symbol) {
+            u32 symbol = 0;
+                symbol < HUFFMAN_MAX_SYMBOLS;
+              ++symbol) {
 
-            const u32 freq = hffmn->freq_table->sparse_array_frequencies[symbol]; 
-            if (freq != 0) {
-                
-                u32& dense_index = freq_table->dense_array.count;
-                freq_table->dense_array.frequencies [dense_index] = freq;
-                freq_table->dense_array.symbols     [dense_index] = symbol;
-                ++dense_index
+
+            const u32 freq = table->sparse_array_frequencies[symbol];
+            if (freq > 0) {
+                u32& dense_index = table->dense_array.count;
+                table->dense_array.frequencies [dense_index] = freq;
+                table->dense_array.symbols     [dense_index] = symbol;
+                ++dense_index;
             }
         }
     }
+    
+    SLD_ALGORITHMS_INTERNAL void
+    huffman_tree_init(
+        huffman_tree*                  tree,
+        huffman_heap*                  heap,
+        const huffman_frequency_table* table) {
 
-    SLD_ALGORITHMS_API_FUNC u32                  huffman_data_stream_memory_size    (const huffman_context* hffmn);
-    SLD_ALGORITHMS_API_FUNC data_stream*         huffman_data_stream_create         (const huffman_context* hffmn);
-    SLD_ALGORITHMS_API_FUNC data_stream*         huffman_data_stream_memory_init    (const u32 data_stream_size, const u32 mem_size, void* mem_ptr);
-    SLD_ALGORITHMS_API_FUNC void                 huffman_copy_data_stream           (const huffman_context* hffmn, data_stream* ds);
+        tree->node_count = 0;
 
+        for (
+            u32 index = 0;
+                index < table->dense_array.count;
+              ++index) {
+
+
+            u32& node_index = tree->node_count++;
+            tree->node_array.frequency  [node_index] = table->dense_array.frequencies [index];
+            tree->node_array.symbol     [node_index] = table->dense_array.symbols     [index];
+            tree->node_array.node_left  [node_index] = HUFFMAN_INVALID;
+            tree->node_array.node_right [node_index] = HUFFMAN_INVALID;
+
+            huffman_heap_push(heap, tree, node_index);
+        }
+    }
+
+    SLD_ALGORITHMS_INTERNAL void
+    huffman_heap_push(
+        huffman_heap* heap,
+        huffman_tree* tree,
+        const u16     node_index) {
+
+        u16 heap_index = heap->size++;
+        
+        heap->data[heap_index] = node_index;
+
+        //TODO(SAM): need fixed upper bound    
+        while (heap_index > 0) {
+
+            u16 push = (heap_index - 1) >> 1;
+
+            const u32* frequency_array = tree->node_array.frequency; 
+            u16&       heap_data_p     = heap->data[push];
+            u16&       heap_data_i     = heap->data[heap_index];
+            
+            if (frequency_array[heap_data_p] <= frequency_array[heap_data_i]) {
+                break;
+            }
+
+            swap_u16(heap_data_p, heap_data_i);
+            heap_data_i = heap_data_p;
+        }
+    }
+
+    SLD_ALGORITHMS_INTERNAL void
+    huffman_heap_pop(
+        huffman_heap* heap,
+        huffman_tree* tree) {
+
+        assert(heap != NULL && tree != NULL);
+
+        u16 min = heap->data[0];
+
+        heap->data[0] = heap->data[--heap->size];
+
+        u16 i = 0;
+
+        while (1) {
+
+            u16 left  = (i * 2) + 1;
+            u16 right = (i * 2) + 2;
+            u16 s     = i;
+
+            const u32  heap_left       = heap->data [left]; 
+            const u32  heap_right      = heap->data [right]; 
+            const u32  heap_s          = heap->data [s]; 
+            const u32* frequency       = tree->node_array.frequency;
+            const u32  frequency_left  = frequency  [heap_left];
+            const u32  frequency_right = frequency  [heap_right]; 
+            const u32  frequency_s     = frequency  [heap_s]; 
+
+            if (left  < heap->size && frequency_left  < frequency_s) s = left;
+            if (right < heap->size && frequency_right < frequency_s) s = right;
+            if (s == i)                                              break;
+
+            swap_u16(heap->data[i], heap->data[s]);
+            i = s;
+        }
+
+        return(min);
+    }
 
 };
--- src/sld-algorithms-internal.hpp
+++ src/sld-algorithms-internal.hpp
@@ -4,41 +4,44 @@
 #include "sld-algorithms.hpp"
 
 #define SLD_ALGORITHMS_API_FUNC
+#define SLD_ALGORITHMS_INTERNAL static
 
 namespace sld {
 
     struct huffman_tree;
     struct huffman_node;
     struct huffman_frequency_table;
+    struct huffman_heap;
 
-    static constexpr byte HUFFMAN_INVALID_SYMBOL    = 0xFF; 
-    static constexpr u32  HUFFMAN_MAX_SYMBOLS       = 256;
-    static constexpr u32  HUFFMAN_MAX_NODES         = (HUFFMAN_MAX_SYMBOLS * 2) - 1;
-    static constexpr u32  HUFFMAN_STRUCT_CONTEXT    = sizeof(huffman_context);
-    static constexpr u32  HUFFMAN_STRUCT_TREE       = sizeof(huffman_tree);
-    static constexpr u32  HUFFMAN_STRUCT_FREQ_TABLE = sizeof(huffman_frequency_table);
+    static constexpr u16 HUFFMAN_INVALID                = 0xFFFF; 
+    static constexpr u16 HUFFMAN_MAX_SYMBOLS            = 256;
+    static constexpr u16 HUFFMAN_MAX_NODES              = (HUFFMAN_MAX_SYMBOLS * 2) - 1;
+    static constexpr u32 HUFFMAN_STRUCT_SIZE_CONTEXT    = sizeof(huffman_context);
+    static constexpr u32 HUFFMAN_STRUCT_SIZE_TREE       = sizeof(huffman_tree);
+    static constexpr u32 HUFFMAN_STRUCT_SIZE_HEAP       = sizeof(huffman_heap);
+    static constexpr u32 HUFFMAN_STRUCT_SIZE_FREQ_TABLE = sizeof(huffman_frequency_table);
     
     struct huffman_context {
         huffman_tree*            tree;
+        huffman_heap*            heap;
         huffman_frequency_table* freq_table;
         byte*                    data_ptr;
         u32                      data_size;
     };
 
     struct huffman_tree {
-        huffman_nodes node_array[HUFFMAN_MAX_NODES];
-        s16           node_count;
-        u16           node_root;
+        u16 node_count;
+        struct {
+            u32 frequency  [HUFFMAN_MAX_NODES];
+            u16 symbol     [HUFFMAN_MAX_NODES];
+            u16 node_left  [HUFFMAN_MAX_NODES];
+            u16 node_right [HUFFMAN_MAX_NODES];
+        } node_array;
     };
 
-    struct huffman_tree_soa {
-        u32 node_count;
-        struct {
-            u32  frequency  [HUFFMAN_MAX_NODES];
-            u16  node_left  [HUFFMAN_MAX_NODES];
-            u16  node_right [HUFFMAN_MAX_NODES];
-            byte symbol     [HUFFMAN_MAX_NODES];
-        } node_array;
+    struct huffman_heap {
+        u16 data[HUFFMAN_MAX_SYMBOLS];
+        u16 size;
     };
 
     struct huffman_node {
@@ -57,6 +60,10 @@ namespace sld {
         } dense_array;
     };
 
+    void huffman_frequency_table_build (huffman_frequency_table* table, const byte* data_ptr, const u32 data_size);
+    void huffman_tree_init             (huffman_tree* tree, const huffman_frequency_table* table);
+    void huffman_heap_push             (huffman_heap* heap, huffman_tree* tree, const u16 node_index);
+    void huffman_heap_pop              (huffman_heap* heap, huffman_tree* tree);
 };
 
 #endif //SLD_ALGORITHMS_INTERNAL_HPP
